1764d8abac9a8e7745b6c55263505deb813198a5
Brian Cardarella <brian@dockyard.com>
2016-07-12 08:24:31 -0400

    Technically working

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
 /* jshint node: true */
 'use strict';
 
+const BasePlugin = require('ember-cli-deploy-plugin');
+const md5Hash = require('./lib/utils/md5-hash');
+const path = require('path');
+const fs = require('fs-promise');
+const mkdirp = require('mkdirp');
+const S3UploadClient = require('./lib/s3');
+
 module.exports = {
-  name: 'ember-cli-deploy-fastboot-s3-downloader-manifest'
+  name: 'ember-cli-deploy-fastboot-s3-downloader-manifest',
+
+  createDeployPlugin(options) {
+    let DeployPlugin = BasePlugin.extend({
+      name: options.name,
+      prepare(context) {
+        let config = context.config['fastboot-s3-downloader-manifest'];
+        let archivePath = config.archivePath;
+        let outputPath = config.outputPath;
+        let ext = this.parseExt(archivePath);
+        let fileName = path.basename(archivePath, ext);
+        let archiveStream = fs.createReadStream(archivePath);
+
+        return md5Hash(archiveStream).then((hash) => {
+          let newFileName = `${fileName}-${hash}${ext}`;
+          let newFilePath = path.join(outputPath, newFileName);
+
+          mkdirp.sync(outputPath);
+          return fs.rename(archivePath, newFilePath).then(() => {
+            let manifestFile =
+`{
+  "bucket": "${config.bucket}",
+  "key": "${newFileName}"
+}`;
+
+            fs.writeFileSync(path.join(outputPath, config.manifestName), manifestFile);
+
+            let s3 = new S3UploadClient(context);
+
+            return {
+              uploadClient: s3,
+              distFiles: [
+                newFileName,
+                config.manifestName
+              ]
+            };
+          });
+        });
+      },
+
+      parseExt(archivePath) {
+        return archivePath.match(/(\.tar\.gz|\.zip|.tar)$/)[0];
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
